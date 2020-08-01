74944868494fb5236d4d2280c07d52bebf703012
Abhiraj Kale <abhirajkale1806@gmail.com>
2020-08-02 00:00:00 +0530

    Initial commit

--- index.php
+++ index.php
@@ -4,8 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chat System</title>
+    <link rel="stylesheet" href="stylesheet/index.css">
 </head>
 <body>
-    hi
+    <center><h1 id="head">Chat System</h1></center>
+<div class="login-page">
+  <div class="form">
+    <form class="register-form" >
+    <h2>Register</h2>
+      <input type="text" placeholder="name"/>
+      <input type="password" placeholder="password"/>
+      <input type="text" placeholder="email address"/>
+      <button>create</button>
+    </form>
+    </div>
+    <div class="form">
+    <form class="login-form">
+    <h2>Login</h2>
+      <input type="text" placeholder="username"/>
+      <input type="password" placeholder="password"/>
+      <button>login</button>
+    </form>
+  </div>
+  <center>~ Created By <a href="https://github.com/abhiraj-kale">Abhiraj Kale</a></center>
+</div>
+
+<script src="js/jquery.js"></script>
+<script src="js/index.js"></script>
 </body>
 </html>
